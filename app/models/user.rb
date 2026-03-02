0182dc7cf4b221b689bcd9401e52bd3ad81e0abc
Zarifa <mamadalievazara@gmail.com>
2026-03-02 23:54:36 +0100

    Create validations for the user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
