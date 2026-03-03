eeebc4bfbe2a870de90969df8e7aef38da31f702
Zarifa <mamadalievazara@gmail.com>
2026-03-04 00:33:23 +0100

    Add validations and associations to a Comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, uniquenes: true, length: { maximum: 200 }
   validates :body, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   has_many :posts
+  has_many :comments, dependent: :destroy
 end
