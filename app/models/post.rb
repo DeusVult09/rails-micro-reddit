63a216ac21070b00c36db435b5c0bd84a5472e7d
Zarifa <mamadalievazara@gmail.com>
2026-03-04 10:03:35 +0100

    Finalize the project

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  validates :title, presence: true, uniquenes: true, length: { maximum: 200 }
+  validates :title, presence: true, uniqueness: true, length: { maximum: 200 }
   validates :body, presence: true
 end
